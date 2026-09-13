af7bac5e40bd66b4f36771c2e2ea565329d001da
kjw8nyst89-rgb <kjw8nyst89@privaterelay.appleid.com>
2026-09-13 12:37:35 +0200

    Update pixeliser.js

--- pixeliser.js
+++ pixeliser.js
@@ -1,5 +1,5 @@
 import createLZFSEModule from "./lzfse/lzfse.js";
-const version="V7-7";
+const version="V7-8";
 // ============================================================
 // VARIABLES GLOBALES
 // ============================================================
